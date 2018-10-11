5bba66881beb2c6dcb252e86ba61e4fe3fbde08c
Joel Edwards <joeledwards@gmail.com>
2018-10-11 16:56:11 -0600

    feat: server now works in both http and https modes (with certs 🎉)

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ Useless
 ## Options
 
 - `FGIP_INCLUDE_COUNTRY` - include a country section in the response
-- `FGIP_BIND_PORT` - bind to this port instead of the default (8080)
+- `FGIP_BIND_PORT` - HTTP server port (default 8080)
+- `FGIP_SECURE_BIND_PORT` - HTTPS server port (default 8443)
+- `FGIP_CERT_PATH` - SSL cert path
+- `FGIP_KEY_PATH` - SSL cert key path
 
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const express = require('express')
 const app = express()
 const bindPort = Number(process.env.FGIP_BIND_PORT) || 8080
+const secureBindPort = Number(process.env.FGIP_SECURE_BIND_PORT) || 8443
 const includeCountry = Boolean(process.env.FGIP_INCLUDE_COUNTRY)
+const certPath = process.env.FGIP_CERT_PATH
+const keyPath = process.env.FGIP_KEY_PATH
 
-app.use((req, res) => {
-  res
-    .status(200)
-    .json({
-      ip: req.ip
-    })
-})
+async function server () {
+  app.use((req, res) => {
+    console.info(`Request from ${req.ip}`)
+    res
+      .status(200)
+      .json({
+        ip: req.ip
+      })
+  })
 
-app.listen(bindPort, () => {
-  console.info(`Listening on ${bindPort}`)
-})
+  http.createServer(app).listen(bindPort, () => {
+    console.info(`Listening on ${bindPort} ...`)
+  })
+
+  var sslOptions = {
+    cert: fs.readFileSync(certPath),
+    key: fs.readFileSync(keyPath),
+    passphrase: 'asdf'
+  };
+
+  https.createServer(sslOptions, app).listen(secureBindPort, () => {
+    console.info(`Listening on ${secureBindPort} ...`)
+  })
+}
+
+server()
